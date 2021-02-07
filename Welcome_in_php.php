57e2eb0a9a60912c6d9a669e1436c230fab574cc
FaisalAnjum4 <54050385+FaisalAnjum4@users.noreply.github.com>
2021-02-07 23:39:28 +0600

    first php

---
+++ Welcome_in_php.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+<?php
+$txt = " PHP Tutorial";
+$x = 5;
+$y = 10;
+$sum = $x*$y;
+echo "Welcome to $txt <br> ";
+echo "The multiplication of $x and $y is $sum";
+?>
+
+</body>
+</html>
